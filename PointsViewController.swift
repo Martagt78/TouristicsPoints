6a88053660a59002880aaa292c1ab314b9d51242
Martagt78 <86956075+Martagt78@users.noreply.github.com>
2021-07-07 09:06:20 +0200

    "Touristic Point v2"

---
+++ PointsViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  TouristicPoints
+//
+//  Created by Marta García on 2/7/21.
+//
+
+import UIKit
+import Foundation
+
+class PointsViewController: UITableViewController, UISearchBarDelegate {
+    
+    var pointsArray = [Places]()
+    
+    //Codigo necesario para ver la transición de la pantalla y mostrar los detalles de cada POI
+    @IBSegueAction func showDetailView(_ coder: NSCoder) -> DetailViewController? {
+        guard let indexPath = tableView.indexPathForSelectedRow else { fatalError("Nothing selected!!")}
+        let detailPoint = dPOI.dpts[indexPath.row]
+        return DetailViewController(coder: coder, detailPoints: detailPoint)
+    }
+    
+    var searchController: UISearchController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Codigo para incluir la barra de busqueda
+        searchController = UISearchController(searchResultsController: nil)
+        searchController?.searchBar.delegate = self
+        searchController?.obscuresBackgroundDuringPresentation = false
+        searchController?.searchBar.placeholder = "Search"
+        tableView.tableHeaderView = searchController?.searchBar
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getPoint()
+       // getDetailPoint(withID: String)
+    }
+    
+    
+    //MARK: - DataSource -
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Método que muestra el numero de celdas que vamos a tener
+        return pointsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(PointCell.self)", for: indexPath) as? PointCell
+        else {fatalError("Could not create PointCell") }
+        let point = pointsArray[indexPath.row]
+        cell.titleLabel?.text = point.list.compactMap({$0.title}).joined(separator: ",")
+        return cell
+    }
+    
+    
+    func getPoint() {
+        
+        let urlPOI = URL(string: "http://t21services.herokuapp.com/points")!
+        var request = URLRequest(url: urlPOI)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: urlPOI) { data, response, error in
+            if let data = data {
+                if let points = try? JSONDecoder().decode(Places.self, from: data) {
+                    self.pointsArray.append(points)
+                } else {
+                    print("Invalid Response")
+                }
+            } else if let error = error {
+                print("HTTP Request Failed \(error)")
+            }
+        }
+        self.tableView.reloadData()
+        task.resume()
+        
+    }
+    
+
+//    func getDetailPoint(withID: String) {
+//
+//        let urlDetailPOI = URL(string: "http://t21services.herokuapp.com/points/\(id.self)")! //Pasar ID que queremos mostrar
+//        var request = URLRequest(url: urlDetailPOI)
+//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//
+//        let task = URLSession.shared.dataTask(with: urlDetailPOI) { data, response, error in
+//            print(data)
+//            print(response)
+//            print(error)
+//
+//            if let data = data {
+//                if let points = try? JSONDecoder().decode(DetailPoints.self, from: data) {
+//                    print(points)
+//                } else {
+//                    print("Invalid Response")
+//                }
+//            } else if let error = error {
+//                print("HTTP Request Failed \(error)")
+//            }
+//
+//        }
+//        task.resume()
+//
+//    }
+}
+
+
